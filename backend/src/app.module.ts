ff47a8a60b676999991199c48b91f478ddd2165a
anaareiis <anarnascente@gmail.com>
2026-07-07 13:50:44 -0300

    feat(db): add PrismaService wrapper (Prisma v7) and DatabaseModule; health endpoint for DB test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
+// backend/src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './prisma.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
